cbb7acecfb77035b353ed57debf927f258e7d156
Łukasz Furman <lukaszfurman@protonmail.com>
2021-05-30 01:35:34 +0200

    add tsne script

--- copied_but_not_used.py
+++ copied_but_not_used.py
@@ -1,12 +1,24 @@
+#import h5py
+#import technicalpha
+#import pandas as pd
+# from sklearn.datasets import fetch_mldata
+# from sklearn.manifold import TSNE
+# %matplotlib inline
+# from mpl_toolkits.mplot3d import Axes3D
+# import seaborn as sns
+#from scipy.io import loadmat
+# from __future__ import print_function
 
-import matplotlib.pyplot as plt
+
 import numpy as np
-from scipy import signal
-import scipy.fftpack
-
+import h5py
+from scipy.spatial.distance import euclidean, pdist, squareform
+import pywt
+import matplotlib.pyplot as plt
 
 from pyrqa.image_generator import ImageGenerator
 from pyrqa.computation import RPComputation
+import numpy as np
 from pyrqa.time_series import TimeSeries
 from pyrqa.settings import Settings
 from pyrqa.analysis_type import Classic
@@ -14,50 +26,34 @@ from pyrqa.neighbourhood import FixedRadius, RadiusCorridor
 from pyrqa.metric import EuclideanMetric
 from pyrqa.computation import RQAComputation
 from pyrqa.neighbourhood import Unthresholded
-
+from scipy import signal
 from nolitsa import dimension
-import pywt
+import numpy as np
 import pickle
-
-
-
-def plotSignal(data):
-  fig,ax = plt.subplots(1,2,figsize=(20,6))
-  ax[0].plot(time,data)
-  ax[0].set_xlabel('Time (s)')
-  ax[0].set_title('Time domain')
-
-  dataX = scipy.fftpack.fft(data/npnts)
-  ax[1].plot(hz,np.abs(dataX[:len(hz)]))
-  ax[1].set_xlim([0,150])
-  ax[1].set_xlabel('Frequency (Hz)')
-  ax[1].set_title('Frequency domain')
-
-
 
 
 if __name__ == '__main__':
 
     filename = 'dane.pkl'
     df=pickle.load(open(filename,'rb'))
-    srate = 256
-    len(df[0])
-    time = np.arange(len(df[0]))
-    npnts = len(time)
-    hz = np.linspace(0, srate / 2, int(npnts / 2) + 1)
-    plotSignal(np.array(df[0]))
-    plt.show()
+
+
     dane = df #FZ
     #matdata = loadmat('sampleEEGdata.mat')
     #matdata.items()
-    elnames = ['Fz']  # C5, C6, Cz, Fz
+    elnames = ['Cz']  # C5, C6, Cz, Fz
+    #f = h5py.File('ALLEEG__20191123_033152_reduced.mat')
+    #print(technicalpha.sublist(f))
 
+    #dane = technicalpha.builder(f, elnames, 'IB2018A0Z63922_rso')
+
+    # timedelays=[8]
     timedel = 8
-
+    # embeddings=[2]
     embedding = 2  # [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-
+    # bins=50
     interval = 1000
-
+    # neighber=1
     fnn_list = []
     tt_list = []
     fnn_dic = {}
@@ -66,22 +62,24 @@ if __name__ == '__main__':
     el_idx = 0
     subject = 'IB2018A0Z63922_rso'
 
-
+    # for el_idx in range(len(elnames)):
     db4 = pywt.Wavelet('db4')
     coeffs = pywt.wavedec(dane[el_idx], db4, mode='periodic', level=5)
     dlu = len(dane[el_idx])
     nazwy = ['delta', 'theta', 'alpha', 'beta', 'gamma', 'high gamma']
-
+    # a=0
     a = 3
-
+    # for i in coeffs :
     f = signal.resample(coeffs[a], dlu)
     if interval:
         list_of_slices = zip(*(iter(f),) * interval)
         counter = 1
     for j in list_of_slices:
-
+        # counter=4
+        # j=f[3000:4000]
         fnn = np.asscalar(dimension.fnn(j, dim=[embedding], tau=timedel, metric='euclidean')[2])
-
+        # for timedel in timedelays:
+        #     for embedding in embeddings:
 
         time_series = TimeSeries(j,
                                  embedding_dimension=embedding,
@@ -96,7 +94,7 @@ if __name__ == '__main__':
                                            verbose=True)
         result = computation.run()
 
-
+        # plt.imshow(np.flip(result.recurrence_matrix_reverse_normalized,1), cmap='jet', interpolation='none', origin='upper')
         plt.imshow(result.recurrence_matrix_reverse_normalized[::-1], cmap='jet', interpolation='none', origin='upper')
         plt.colorbar()
         plt.gca().invert_yaxis()
@@ -137,13 +135,14 @@ if __name__ == '__main__':
     ax2.scatter(timestamps, tt_list, color="blue", alpha=0.3)
     ax.set_ylabel("FNN", color="red")
     ax2.set_ylabel("TT", color="blue", rotation=270)
-
+    # ax.set_yticks(np.linspace(np.min(fnn_dic["C6"][band]), np.max(fnn_dic["C6"][band]),10))
+    # ax2.set_yticks(np.linspace(np.min(tt_dic["C6"][band]), np.max(tt_dic["C6"][band]),10))
     ax.set_yticks(np.round(np.linspace(np.min(fnn_list), np.max(fnn_list), 10), 2))
     ax2.set_yticks(np.round(np.linspace(np.min(tt_list), np.max(tt_list), 10), 2))
     plt.xticks(timestamps)
     ax.set_xlabel('Timestamps')
     plt.title("Electrode C6 " + "beta" + " band")
-
+    # plt.show()
     plt.savefig("FNN_TT" + "_C6_beta_" + '.png')
     plt.close()
 
@@ -151,5 +150,3 @@ if __name__ == '__main__':
     plt.xlabel("Trapping times")
     plt.ylabel("Fnns")
     plt.show()
-
-
