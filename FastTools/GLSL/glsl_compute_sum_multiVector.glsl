af1aeba961d95ce8fa4e12618bbf2c92b7764396
Łukasz Furman <lukaszfurman@protonmail.com>
2021-07-20 21:29:18 +0200

    update SFT/RP Vector

--- FastTools/GLSL/glsl_compute_sum_multiVector.glsl
+++ FastTools/GLSL/glsl_compute_sum_multiVector.glsl
@@ -21,12 +21,22 @@ return sum;
 layout (local_size_x = 49, local_size_y = 1) in;
 void main()
 {
-	vec4 color;
+	vec4 color, sftVectors, sftVectorsShift;
 	color = texelFetch(sTD2DInputs[0], ivec2(gl_GlobalInvocationID.xy), 0);
 	
+	sftVectors = texelFetch(sTD2DInputs[0], ivec2(gl_GlobalInvocationID.xy), 0);
+	sftVectorsShift = texelFetch(sTD2DInputs[0], ivec2(gl_GlobalInvocationID.x,gl_GlobalInvocationID.y-1), 0);
 	imageStore(mTDComputeOutputs[0], ivec2(gl_GlobalInvocationID.xy), TDOutputSwizzle(color));
 
 	float sumFFT = sum(0);
 	imageStore(mTDComputeOutputs[1], ivec2(0,gl_GlobalInvocationID.y), TDOutputSwizzle(vec4(sumFFT,0,0,1)));
+	float maxSFTVector = max(sftVectors.r, sftVectorsShift.r);
+	float DistSFT_Vectors = (sftVectors.r-sftVectorsShift.r);
+	DistSFT_Vectors = length(DistSFT_Vectors);
+	// DistSFT_Vectors *= abs(sftVectors.r-sftVectorsShift.r);
+	DistSFT_Vectors *= maxSFTVector;
+	//float DistSFT_Vectors = length((sftVectors.r-sftVectorsShift.r)*abs(sftVectors.r-sftVectorsShift.r));
+	//DistSFT_Vectors = length(sftVectors.r-sftVectorsShift.r)*abs(sftVectors.r-sftVectorsShift.r);
+	imageStore(mTDComputeOutputs[2], ivec2(gl_GlobalInvocationID.xy), TDOutputSwizzle(vec4(DistSFT_Vectors,0,0,1)));
 
 }
