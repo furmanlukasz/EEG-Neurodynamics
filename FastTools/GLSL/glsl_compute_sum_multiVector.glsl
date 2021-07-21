fec3f56c81fcd194d2a29e1b1058f51d6f81e312
Łukasz Furman <lukaszfurman@protonmail.com>
2021-07-21 20:32:06 +0200

    update on RP/SFT
    changed:
    window range hyperparameter

--- FastTools/GLSL/glsl_compute_sum_multiVector.glsl
+++ FastTools/GLSL/glsl_compute_sum_multiVector.glsl
@@ -8,6 +8,7 @@
 //in uvec3 gl_GlobalInvocationID;
 //in uint  gl_LocalInvocationIndex;
 
+//idx - glsltop input index
 float sum(int idx){
 	float sum = 0;
 	int k = 0;
@@ -21,22 +22,26 @@ return sum;
 layout (local_size_x = 49, local_size_y = 1) in;
 void main()
 {
-	vec4 color, sftVectors, sftVectorsShift;
-	color = texelFetch(sTD2DInputs[0], ivec2(gl_GlobalInvocationID.xy), 0);
+	vec4 sftVectors, sftVectorsShift;
+	
 	
 	sftVectors = texelFetch(sTD2DInputs[0], ivec2(gl_GlobalInvocationID.xy), 0);
 	sftVectorsShift = texelFetch(sTD2DInputs[0], ivec2(gl_GlobalInvocationID.x,gl_GlobalInvocationID.y-1), 0);
-	imageStore(mTDComputeOutputs[0], ivec2(gl_GlobalInvocationID.xy), TDOutputSwizzle(color));
 
+	// output orginal vectors
+	imageStore(mTDComputeOutputs[0], ivec2(gl_GlobalInvocationID.xy), TDOutputSwizzle(sftVectors));
+
+	// calc and output sum of FFTvector in Y
 	float sumFFT = sum(0);
 	imageStore(mTDComputeOutputs[1], ivec2(0,gl_GlobalInvocationID.y), TDOutputSwizzle(vec4(sumFFT,0,0,1)));
+	
+	// calc and output dist betwen vector FFT(tn) and vector FFT(tn-1)  
 	float maxSFTVector = max(sftVectors.r, sftVectorsShift.r);
 	float DistSFT_Vectors = (sftVectors.r-sftVectorsShift.r);
 	DistSFT_Vectors = length(DistSFT_Vectors);
-	// DistSFT_Vectors *= abs(sftVectors.r-sftVectorsShift.r);
 	DistSFT_Vectors *= maxSFTVector;
-	//float DistSFT_Vectors = length((sftVectors.r-sftVectorsShift.r)*abs(sftVectors.r-sftVectorsShift.r));
-	//DistSFT_Vectors = length(sftVectors.r-sftVectorsShift.r)*abs(sftVectors.r-sftVectorsShift.r);
+	// DistSFT_Vectors *= abs(sftVectors.r-sftVectorsShift.r);
+	
 	imageStore(mTDComputeOutputs[2], ivec2(gl_GlobalInvocationID.xy), TDOutputSwizzle(vec4(DistSFT_Vectors,0,0,1)));
 
 }
