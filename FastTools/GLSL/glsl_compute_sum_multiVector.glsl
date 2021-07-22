b7a3acaf15b9f72725c866d5705ba600f6ac6ee4
Łukasz Furman <lukaszfurman@protonmail.com>
2021-07-22 21:07:15 +0200

    update sumW function

--- FastTools/GLSL/glsl_compute_sum_multiVector.glsl
+++ FastTools/GLSL/glsl_compute_sum_multiVector.glsl
@@ -9,6 +9,9 @@
 //in uint  gl_LocalInvocationIndex;
 
 //idx - glsltop input index
+
+
+
 float sum(int idx){
 	float sum = 0;
 	int k = 0;
@@ -35,6 +38,8 @@ void main()
 	float sumFFT = sum(0);
 	imageStore(mTDComputeOutputs[1], ivec2(0,gl_GlobalInvocationID.y), TDOutputSwizzle(vec4(sumFFT,0,0,1)));
 	
+
+	
 	// calc and output dist betwen vector FFT(tn) and vector FFT(tn-1)  
 	float maxSFTVector = max(sftVectors.r, sftVectorsShift.r);
 	float DistSFT_Vectors = (sftVectors.r-sftVectorsShift.r);
@@ -43,5 +48,7 @@ void main()
 	// DistSFT_Vectors *= abs(sftVectors.r-sftVectorsShift.r);
 	
 	imageStore(mTDComputeOutputs[2], ivec2(gl_GlobalInvocationID.xy), TDOutputSwizzle(vec4(DistSFT_Vectors,0,0,1)));
+
+	
 
 }
